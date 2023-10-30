53bf64d7c44f9ab7421a0cc83e7b55a982a23ff6
renzhonglu11 <lu631992923q@gmail.com>
2023-10-30 18:05:36 +0100

    exe_3 not finish

--- src/main.c
+++ src/main.c
@@ -10,13 +10,69 @@ void initial_interrupt()
     RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
     SYSCFG->EXTICR[3] = SYSCFG_EXTICR4_EXTI13_PC;
     EXTI->IMR |= EXTI_IMR_IM13;
-
     EXTI->FTSR |= EXTI_FTSR_TR13;
+
 
     NVIC_ClearPendingIRQ(EXTI4_15_IRQn);
     NVIC_SetPriority(EXTI4_15_IRQn,1);
-
     NVIC_EnableIRQ(EXTI4_15_IRQn);
+}
+
+void TIM3_PWM_Init()
+{
+    RCC->APB1RSTR &= ~RCC_APB1RSTR_TIM2RST;
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // enable timer2
+
+
+    TIM2->PSC  = 16000-1;
+    TIM2->ARR = 500-1; 
+
+    // set to 111
+    TIM2->CCMR1 |= TIM_CCMR1_OC1M_0;
+    TIM2->CCMR1 |= TIM_CCMR1_OC1M_1;
+    TIM2->CCMR1 |= TIM_CCMR1_OC1M_2; 
+
+    TIM2->CCER |= TIM_CCER_CC1E; // enable capture
+
+
+    // set GPIOA to output
+    GPIOA->MODER |= GPIO_MODER_MODE5_0;
+    GPIOA->MODER &= ~(GPIO_MODER_MODE5_1); 
+
+    TIM2-> |= TIM_CR1_ARPE; // auto reload enable
+    TIM2->CR1 |= TIM_CR1_CEN; // time2 enable
+    
+}
+
+
+void TIM2_Int_Init()
+{
+    // RCC->APB1RSTR |= RCC_APB1RSTR_TIM2RST;
+    RCC->APB1RSTR &= ~RCC_APB1RSTR_TIM2RST;
+
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // enable timer2
+    
+    TIM2->PSC  = 16000-1;
+    TIM2->ARR = 500-1; // auto reload register
+
+    TIM2->DIER |= TIM_DIER_UIE; // DMA
+
+    NVIC_ClearPendingIRQ(TIM2_IRQn);
+    NVIC_SetPriority(TIM2_IRQn,1);
+    NVIC_EnableIRQ(TIM2_IRQn); // enable NVIC interrupt for timer 2
+
+    TIM2->CR1 |= TIM_CR1_CEN;
+
+}
+
+void TIM2_IRQHandler( void )
+{
+    if(TIM2->SR & TIM_SR_UIF)
+    {
+        GPIOA->ODR ^= GPIO_BSRR_BS_5;
+        
+        TIM2->SR &= ~TIM_SR_UIF;
+    }
 }
 
 
@@ -48,7 +104,7 @@ int32_t max_value(int16_t *data, uint8_t len,int16_t *max)
         }
     }
 
-    return *max;
+    return *max; // using the value from template as success code
 }
 
 
@@ -149,8 +205,10 @@ int main(void)
     RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // enable clock for SYSCFG
    
     
-    GPIOA->MODER |= GPIO_MODER_MODE5_0;
-    GPIOA->MODER &= ~(GPIO_MODER_MODE5_1); 
+    // GPIOA->MODER |= GPIO_MODER_MODE5_0;
+    // GPIOA->MODER &= ~(GPIO_MODER_MODE5_1); 
+
+
 
 
     GPIOC->MODER &= ~GPIO_MODER_MODE13;  // input
@@ -158,10 +216,8 @@ int main(void)
     // Enable Pull-Up
     // GPIOC->PUPDR |= GPIO_PUPDR_PUPD13_0; // Set LSB => 0b…XXXX1
     // GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD13_1); // Reset MSB => 0b…XXX0X
-
-
-
-    initial_interrupt();
+    // initial_interrupt();
+    TIM2_Int_Init();
 
     const char text[] = "Hello, World!\n";
     int16_t data[] = {0, 123, 42, -399};
@@ -208,18 +264,19 @@ int main(void)
         //     GPIOA->BRR = GPIO_BSRR_BS_5;
         // }
 
-        int16_t my_max = 0;
-        int16_t *max_ptr;
-        max_value(data,4,&my_max);
-        max_ptr = max_value_ptr(data,4);
+        // int16_t my_max = 0;
+        // int16_t *max_ptr;
+        // max_value(data,4,&my_max);
+        // max_ptr = max_value_ptr(data,4);
         
 
-        *max_ptr = 1337; // change the corresponding value in data array
-        uint16_t max_len = 0;
-        char max_buf[20];
-        int2string(max_buf, 10, *max_ptr, &max_len);
-        uart_tx_str(max_buf);
-        uart_tx_str("\n");
+        // *max_ptr = 1337; // change the corresponding value in data array
+
+        // uint16_t max_len = 0;
+        // char max_buf[20];
+        // int2string(max_buf, 10, *max_ptr, &max_len);
+        // uart_tx_str(max_buf);
+        // uart_tx_str("\n");
          
         // int data_len = sizeof(data) / sizeof(data[0]);
         // print_array(data,data_len);
@@ -239,9 +296,9 @@ int main(void)
             
         // }
         
+        // GPIOA->BRR = GPIO_BSRR_BS_5; // LED OFF
 
 
-
-        systick_delay_ms(1000);
+        // systick_delay_ms(1000);
     }
 }
