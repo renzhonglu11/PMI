b10687e63a8549a7857517d7fd5b5f489397feb8
renzhong <lu631992923q@gmail.com>
2023-10-30 01:30:54 +0100

    exe_2 is done

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include <clocks.h>
 #include <stm32l0xx.h>
 #include <systick.h>
-
-
+#include <stdio.h>
+#include <inttypes.h>
 
 void initial_interrupt()
 {
@@ -31,6 +31,113 @@ void EXTI4_15_IRQHandler(void)
 }
 
 
+int32_t max_value(int16_t *data, uint8_t len,int16_t *max)
+{
+    if(len == 0)
+    {
+        return 0; // len darf nicht 0 sein.
+    }
+
+    *max = data[0];
+
+    for(int i = 0;i<len;i++)
+    {
+        if(data[i] > *max)
+        {
+            *max = data[i];
+        }
+    }
+
+    return *max;
+}
+
+
+int16_t *max_value_ptr(int16_t *data, uint8_t len)
+{
+    if(len == 0)
+    {
+        return 0; // len darf nicht 0 sein.
+    }
+
+    int16_t *max = &data[0]; // Pointer auf das erste Element.
+
+    for(int i = 0;i<len;i++)
+    {
+        if(data[i] > *max)
+        {
+            max = &data[i];
+        }
+    }
+
+    return max;
+}
+
+
+void print_array(int16_t *data, uint8_t len)
+{
+    for(int i = 0;i<len;i++)
+    {
+        char charArray[20];
+        sprintf(charArray, "%" PRId16, data[i]);
+        
+        uart_tx_str(charArray);
+        if(i == len - 1)
+        {
+            uart_tx_str("\n");
+            continue;
+        }
+        uart_tx_str(", ");
+        
+    }
+}
+
+
+int32_t int2string(char *buf, uint16_t size, int16_t number,uint16_t *len)
+{
+    int i = 0;
+    int is_neg = 0;
+
+    if(number<0)
+    {
+      is_neg = 1;
+      number = -number;
+    }
+
+    do {
+        buf[i++] = '0' + (number % 10);
+        number /= 10;
+    } while (number > 0);
+
+    if (is_neg) {
+        buf[i++] = '-';
+    }
+
+    if(i>size)
+    {
+        return -1; // buffer overflow
+    }
+
+    buf[i] = '\0'; // end string
+
+
+    // reverse string
+    int length = i;
+    for(int j=0;j<length/2;j++)
+    {
+        char tmp = buf[j];
+        buf[j] = buf[length-j-1];
+        buf[length-j-1] = tmp;
+    }
+
+
+    *len = length;
+
+    return length;
+}
+
+
+
+
 int main(void)
 {
     /* Call your initialisations here */
@@ -41,7 +148,7 @@ int main(void)
     RCC->IOPENR |= RCC_IOPENR_GPIOCEN;
     RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // enable clock for SYSCFG
    
-
+    
     GPIOA->MODER |= GPIO_MODER_MODE5_0;
     GPIOA->MODER &= ~(GPIO_MODER_MODE5_1); 
 
@@ -57,7 +164,7 @@ int main(void)
     initial_interrupt();
 
     const char text[] = "Hello, World!\n";
-
+    int16_t data[] = {0, 123, 42, -399};
 
 
 
@@ -100,8 +207,41 @@ int main(void)
         // }else{
         //     GPIOA->BRR = GPIO_BSRR_BS_5;
         // }
+
+        int16_t my_max = 0;
+        int16_t *max_ptr;
+        max_value(data,4,&my_max);
+        max_ptr = max_value_ptr(data,4);
         
 
-        // systick_delay_ms(1000);
+        *max_ptr = 1337; // change the corresponding value in data array
+        uint16_t max_len = 0;
+        char max_buf[20];
+        int2string(max_buf, 10, *max_ptr, &max_len);
+        uart_tx_str(max_buf);
+        uart_tx_str("\n");
+         
+        // int data_len = sizeof(data) / sizeof(data[0]);
+        // print_array(data,data_len);
+        
+        // for(int i = 0;i<data_len;i++)
+        // {
+        //     char charArray[20];
+        //     sprintf(charArray, "%" PRId16, data[i]);
+            
+        //     uart_tx_str(charArray);
+        //     if(i == data_len - 1)
+        //     {
+        //         uart_tx_str("\n");
+        //         continue;
+        //     }
+        //     uart_tx_str(", ");
+            
+        // }
+        
+
+
+
+        systick_delay_ms(1000);
     }
 }
