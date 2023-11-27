bd464ba22552c11027653b29e5aaf545260d1781
renzhonglu11 <lu631992923q@gmail.com>
2023-11-27 16:32:55 +0100

    initialise interrupt

---
+++ include/uart_irq.h
@@ -0,0 +1,7 @@
+#ifndef UART_IRQ_H
+#define UART_IRQ_H
+
+#include <pmi_stddefs.h>
+
+
+#endif /* UART_IRQ_H */
---
+++ src/uart_irq/uart_irq.c
@@ -0,0 +1,6 @@
+#include <uart.h>
+#include <stm32l0xx.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+
