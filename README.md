199fbeda962dab8eb911d20e2455905ae1a1ab1f
renzhonglu11 <lu631992923q@gmail.com>
2024-01-10 16:01:21 +0100

    try to implement ADC

--- README.md
+++ README.md
@@ -8,3 +8,14 @@
 - prescale?
 - GPIO analog input?
 - 
+
+
+### ADC registers
+
+ADC1->CR:
+ADEN: check if ADC is enable
+ADSTART: check if ADC starts
+
+
+ADC1->ISR: status register
+EOCAL: bit is set by hardware when calibration completes
--- src/adc/adc.c
+++ src/adc/adc.c
@@ -19,23 +19,50 @@ uint32_t adc_init()
 
   // ensure adc is disabled
 
+
+  // using clock that is derived from APB clock
   // clear clock mode first
 
+  // select clock mode
+  ADC1->CFGR2 &= (~ADC_CFGR2_CKMODE); // clear mode bits 
+  ADC1->CFGR2 |= ADC_CFGR2_CKMODE_0;
+  
 
   // set alignment to right (set ADSTART to 0 first)
-
-
   
 
   // enable adc
 
+
+
   /*
     calibration:
-      1. enable calibration
-      2. start calibration
-      3. wait for calibration to finish
+      1. Ensure that ADEN = 0 and DMAEN = 0.
+      2. Set ADCAL = 1.
+      3. Wait until ADCAL = 0 (or until EOCAL = 1). This can be handled by interrupt if the 
+      interrupt is enabled by setting the EOCALIE bit in the ADC_IER register. The ADCAL 
+      bit can remain set for some time even after EOCAL has been set. As a result, the 
+      software must wait for ADCAL = 0 after EOCAL = 1 to be able to set ADEN = 1 for next 
+      ADC conversions.
+      4. The calibration factor can be read from bits 6:0 of ADC_DR or ADC_CALFACT 
+      registers.
   */
 
+  // ensure ADEN to be 0 before calibration
+  if((ADC1->CR & ADC_CR_ADEN)!=0)
+  {
+    ADC1->CR |= ADC_CR_ADDIS;    // disable ADC
+  }
+
+  ADC1->CR |= ADC_CR_ADCAL;    // start calibration
+
+  while ((ADC1->ISR & ADC_ISR_EOCAL)==0)
+  {
+    
+  }
+
+  ADC1->ISR |= ADC_ISR_EOCAL;  // write 1 to clear bit 
+  
 
 
   return RC_SUCC;
