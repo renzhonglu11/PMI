0c0bd46085d84990902cddcb977d178084e54c34
renzhonglu11 <lu631992923q@gmail.com>
2024-01-22 17:49:49 +0100

    refactor codes

--- include/pmi.h
+++ include/pmi.h
@@ -9,17 +9,13 @@
 #include <graph_utils.h>
 #include <pmi_string.h>
 #include <stdio.h>
+#include "timer.h"
+#include "global_common.h"
 
 
-extern const float DIVISOR_MV;
-extern uint8_t rc_range;
-extern uint32_t p2p_val;
-extern volatile uint8_t zoom_lvl;
 
 
-#define BUFFER_SIZE 240
-#define PRE_TRIGGER_COUNT 120
-#define POST_TRIGGER_COUNT 120
+
 
 uint16_t adc_buffer[BUFFER_SIZE];
 uint16_t extracted_data[BUFFER_SIZE];
@@ -29,9 +25,7 @@ uint16_t extracted_data[BUFFER_SIZE];
 extern uint8_t graph_ready;
 extern uint8_t data_ready;
 
-uint32_t TIM6_init();
-uint32_t TIM2_init();
-uint32_t TIM21_init();
+
 uint32_t initialize_gpio();
 uint32_t extract_samples(uint16_t *extracted_data);
 
